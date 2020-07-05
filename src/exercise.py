a4a88290076bf6ae81409362bdcd8913d58ecc2b
Vladimir Slysar <g7au2ss@gmail.com>
2020-07-06 03:02:46 +0700

    1

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,21 @@
 def main():
-    #write your code below this line
+    pos = 0
+    neg = 0
+    point = 0
+    while True:
+        num = int(input())
+        if num == 0:
+            break
+        elif num>0:
+            pos = pos + num
+            point += 1
+        elif num<0:
+            neg = neg + num
+    if pos == 0:
+        print("Cannot calculate the average")
+    else:
+        print(pos/point)
+
 
 if __name__ == '__main__':
     main()
